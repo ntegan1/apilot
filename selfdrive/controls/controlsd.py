20f05e1fd08f40050bd686ea7ca43111e31ee571
ntegan1 <--global>
2022-09-22 14:15:06 -0400

    steerhere

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -600,8 +600,7 @@ class Controls:
     CC = car.CarControl.new_message()
     CC.enabled = self.enabled
     # Check which actuators can be enabled
-    CC.latActive = self.active and not CS.steerFaultTemporary and not CS.steerFaultPermanent and \
-                   CS.vEgo > self.CP.minSteerSpeed and not CS.standstill
+    CC.latActive = self.active and not CS.steerFaultTemporary and not CS.steerFaultPermanent
     CC.longActive = self.active and not self.events.any(ET.OVERRIDE_LONGITUDINAL) and self.CP.openpilotLongitudinalControl
 
     actuators = CC.actuators
