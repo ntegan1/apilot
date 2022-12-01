8a14f1684b9db03c41e99bea44590a09c4a200ea
nick <nick@egan.gg>
2022-12-01 15:51:31 -0500

    .

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -54,7 +54,6 @@ procs = [
   PythonProcess("thermald", "selfdrive.thermald.thermald", offroad=True),
   PythonProcess("tombstoned", "selfdrive.tombstoned", enabled=not PC, offroad=True),
   PythonProcess("updated", "selfdrive.updated", enabled=not PC, onroad=False, offroad=True),
-  PythonProcess("uploader", "selfdrive.loggerd.uploader", offroad=True),
   PythonProcess("statsd", "selfdrive.statsd", offroad=True),
 
   # debug procs
