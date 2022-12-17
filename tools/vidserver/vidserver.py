0beee75f96d4315ffff896bb631df46eb587707f
ntegan1 <--global>
2022-12-16 20:50:17 -0500

    factor out mp4 wrapping of raw log videos

--- tools/vidserver/vidserver.py
+++ tools/vidserver/vidserver.py
@@ -3,9 +3,11 @@ import os
 import subprocess
 
 from flask import Flask, Response, request
-from selfdrive.loggerd.config import ROOT
+#from selfdrive.loggerd.config import ROOT
 from selfdrive.loggerd.uploader import listdir_by_creation
 from tools.lib.route import SegmentName
+
+ROOT = "/home/ntegan/2022_12_16_tacov1/data/"
 
 def is_valid_segment(segment):
   try:
@@ -39,42 +41,44 @@ def segments_in_route(route):
   segments = [segment_name.time_str + "--" + str(segment_name.segment_num) for segment_name in segment_names]
   return segments
 
+def ffmpeg_mp4_wrap_process(filename):
+  """Returns a process that will wrap the given filename
+     inside an mp4 container, for easier playback by browsers
+     and other devices
+
+     filename is expected to be a pathname to one of the following
+       /path/to/a/qcamera.ts
+       /path/to/a/dcamera.hevc
+       /path/to/a/ecamera.hevc
+       /path/to/a/fcamera.hevc
+  """
+  basename = filename.rsplit("/")[-1]
+  extension = basename.rsplit(".")[-1]
+  command_line = ["ffmpeg"]
+  if extension == "hevc":
+    command_line += ["-f", "hevc"]
+  command_line += ["-r", "20"]
+  command_line += ["-i", filename]
+  command_line += ["-c", "copy"]
+  command_line += ["-map", "0"]
+  if extension == "hevc":
+    command_line += ["-vtag", "hvc1"]
+  command_line += ["-f", "mp4"]
+  command_line += ["-movflags", "empty_moov"]
+  command_line += ["-"]
+  return subprocess.Popen(
+    command_line, stdout=subprocess.PIPE
+  )
+
 app = Flask(__name__,)
 
 @app.route("/<cameratype>/<segment>")
 def fcamera(cameratype, segment):
   if not is_valid_segment(segment):
     return "invalid segment"
+  file_name = ROOT + "/" + segment + "/" + cameratype + (".ts" if cameratype == "qcamera" else ".hevc")
 
-  if cameratype in ['fcamera', 'dcamera', 'ecamera']:
-    proc = subprocess.Popen(
-      ["ffmpeg",
-        "-f", "hevc",
-        "-r", "20",
-        "-i", ROOT + "/" + segment + "/" + cameratype + ".hevc",
-        "-c", "copy",
-        "-map", "0",
-        "-vtag", "hvc1",
-        "-f", "mp4",
-        "-movflags", "empty_moov",
-        "-",
-      ], stdout=subprocess.PIPE
-    )
-  elif cameratype in ['qcamera']:
-    proc = subprocess.Popen(
-      ["ffmpeg",
-        "-r", "20",
-        "-i", ROOT + "/" + segment + "/qcamera.ts",
-        "-c", "copy",
-        "-map", "0",
-        "-f", "mp4",
-        "-movflags", "empty_moov",
-        "-",
-      ], stdout=subprocess.PIPE
-    )
-  else:
-    return "invalid camera type"
-  return Response(proc.stdout.read(), status=200, mimetype='video/mp4')
+  return Response(ffmpeg_mp4_wrap_process(file_name).stdout.read(), status=200, mimetype='video/mp4')
 
 @app.route("/<route>")
 def route(route):
@@ -113,7 +117,6 @@ def route(route):
   </body>
     <script>
     var video = document.getElementById('video');
-
     var tracks = {
       list: ["""+segments+"""],
       index: """+query_segment+""",
@@ -127,7 +130,6 @@ def route(route):
         return ( \""""+query_type+"""/" + this.list[this.index] );
       }
     }
-
     video.addEventListener('ended', function(e) {
       tracks.next();
       video.src = tracks.play();
@@ -136,7 +138,6 @@ def route(route):
       video.load();
       video.play();
     });
-
     video.src = tracks.play();
     document.getElementById("currentsegment").textContent=video.src.split("/")[4];
     document.getElementById("currentview").textContent=video.src.split("/")[3];
