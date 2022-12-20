42267ebc6543d9c286f0485d2cf6efae8cb2cd70
ntegan1 <--global>
2022-12-20 00:49:28 -0500

    set atarget for later debugging

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -774,6 +774,7 @@ class Controls:
     controlsState.vPid = float(self.LoC.v_pid)
     controlsState.vCruise = float(self.v_cruise_helper.v_cruise_kph)
     controlsState.vCruiseCluster = float(self.v_cruise_helper.v_cruise_cluster_kph)
+    controlsState.aTarget = float(self.c.get_accel(-100., 100.))
     controlsState.upAccelCmd = float(self.LoC.pid.p)
     controlsState.uiAccelCmd = float(self.LoC.pid.i)
     controlsState.ufAccelCmd = float(self.LoC.pid.f)
