5bb9d5b863605e0f8cbc1244ee98d94cc4e95fda
ntegan <ntegan1@gmail.com>
2023-03-17 17:24:33 -0400

    no gnuplot on c3

--- maneuver.py
+++ maneuver.py
@@ -20,8 +20,6 @@ script = """ \
         plot data using 1:3 title columnhead(3) with lines lc rgbcolor "0x00c9211a", data using ($1):(2.23694*($2)) title "".columnhead(2)." (mph)" with lines lc rgbcolor "0x001f77b4";
 """
 
-os.system("gnuplot -p -e '" + script + "'")
-exit()
 import cereal.messaging as messaging
 
 pm = messaging.PubMaster(['testJoystick'])
